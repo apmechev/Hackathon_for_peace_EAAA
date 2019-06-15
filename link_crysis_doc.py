95e5ee4d50afa715d5fe615f585c7d7f11b78ba6
De Rybel Aaron <aaron.de.rybel@howest.be>
2019-06-15 23:37:50 +0200

    small

--- link_crysis_doc.py
+++ link_crysis_doc.py
@@ -1,10 +1,10 @@
 from get_disaster_data import *
-
+import pandas as pd
 
 APP_NAME= 'EA4'
 
 LATEST_EVENTS = "https://api.reliefweb.int/v1/disasters?appname={}&preset=latest".format(APP_NAME)
 
 data = get_n_latest_events_data()
-
+df = pd.DataFrame(data)
 print('debug')
